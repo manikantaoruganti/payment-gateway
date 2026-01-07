f6b09ab5dd06a4e3ebe1efddd0433d9008f95de8
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2026-01-07 21:05:03 +0530

    Add Failure component and route to checkout-page App

--- checkout-page/src/App.jsx
+++ checkout-page/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Checkout from './pages/Checkout';
 import Success from './pages/Success';
+import Failure from './pages/Failure';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/success" element={<Success />} />
         <Route path="/" element={<Checkout />} />
+                <Route path="/failure" element={<Failure />} />
       </Routes>
     </Router>
   );
